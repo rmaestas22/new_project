db6fabef6741292b148b5476a3a67be3868e738d
Ray Maestas <rjmaestas1991@gmail.com>
2022-06-08 12:30:18 -0600

    Add explanations to arrays

--- arrays.rb
+++ arrays.rb
@@ -6,11 +6,14 @@ due = [10.5, 20.5, 30.5, 40.5]
 
 finished = [true, true, false, false]
 
-
+pop removes the last element
 students.pop
 
+pushadds to the end
 paid.push(50, 60)
 
+shift removes the first element
 due.shift
 
+unshitf add to the beginign
 finished.unshift(true, false)
